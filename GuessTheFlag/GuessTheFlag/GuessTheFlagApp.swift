8acebd1f9c75b8e95b7b8ee23526ba8bb8f5a5d3
Ivaylo Ivanov <105466645+ivayloivan0v@users.noreply.github.com>
2022-10-19 21:58:09 +0300

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ivaylo Ivanov on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
