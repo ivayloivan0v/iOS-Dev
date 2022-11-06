d0f4613df8af5d526fe18a9c25b026ec65ff04e8
Ivaylo <ivayloivan0v@icloud.com>
2022-11-06 19:16:17 +0200

    finishing training project 1

--- UIKit/StormViewer/StormViewer/DetailViewController.swift
+++ UIKit/StormViewer/StormViewer/DetailViewController.swift
@@ -11,12 +11,32 @@ class DetailViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
     var selectedImage: String?
+    var selectedPictureNumber: Int?
+    var totalPictures: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Picture \(String(describing: selectedPictureNumber!)) of \(String(describing: totalPictures!))"
+        navigationItem.largeTitleDisplayMode =  .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
     
 
     /*
