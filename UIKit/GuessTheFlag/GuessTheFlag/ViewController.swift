0341262c0cc09047515b4dc06db905f963404740
Ivaylo <ivayloivan0v@icloud.com>
2022-11-06 21:06:44 +0200

    designing the layout

---
+++ UIKit/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Ivaylo Ivanov on 11/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
